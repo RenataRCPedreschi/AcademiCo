f79c44bbc83ce79f17cddb4057356220e8d1fff7
RenataRCPedreschi <renatarabelocastro@gmail.com>
2023-04-18 21:21:23 -0300

    Esqueleto do projeto

---
+++ index.js
@@ -0,0 +1,26 @@
+//Importações principais e variáveis de ambiente
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+
+//Configuração APP
+const app = express();
+app.use(express.json()); //Possibilitar transitar dados usando JSON
+app.use(morgan("dev"));
+
+//Configuração do Banco de dados
+const { connection, authenticate } = require("./database/database");
+authenticate(connection); //efetivar a conexão
+
+//Definição de rotas
+
+
+
+
+//Escuta dos eventos(listen)
+app.listen(3000, () => {
+  //Gerar as tabelas a partir do model
+  //Force = apaga tudo e recria as tabelas
+  connection.sync(/* { force: true } */);
+  console.log("Servidor rodando em http://localhost:3000");
+});
