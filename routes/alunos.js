cfe66e4a546cb9e7f0ff4a1ee9ac5d80d81c406f
JoseAugustoJasfarias <josejasfarias@gmail.com>
2023-04-20 12:05:27 -0300

    Criação do Delete CRUD

--- routes/alunos.js
+++ routes/alunos.js
@@ -4,24 +4,24 @@ const Turma = require("../database/turma");
 
 const router = Router();
 
-router.get("/alunos", async (req, res) => {
+router.get('/alunos', async (req, res) => {
   const listaAlunos = await Aluno.findAll();
   res.status(200).json(listaAlunos);
-})
+});
 
-
-router.get("/alunos/:id", async (req, res) => {
+router.get('/alunos/:id', async (req, res) => {
   const aluno = await Aluno.findOne({
     where: { id: req.params.id }
-  })
+  });
 
   if (aluno) {
-    res.status(200).json(aluno)
+    res.status(200).json(aluno);
   } else {
-    res.status(404).json({ message: "Aluno não encontrado." })
+    res.status(404).json({ message: 'Aluno não encontrado.' });
   }
+});
 
-})
+
 
 router.post("/alunos", async (req, res) => {
   const { nome, dataNasc, telefone, email, numMatr, turmaId } = req.body;
@@ -40,8 +40,9 @@ router.post("/alunos", async (req, res) => {
     console.error(error);
     res.status(500).json({ message: "Matrícula existente, Impossível cadastrar o mesmo aluno." })
   }
-})
-router.put("/alunos/:id", async (req, res) => {
+});
+
+router.put('/alunos/:id', async (req, res) => {
   const { nome, dataNasc, telefone, email, numMatr } = req.body;
   const aluno = await Aluno.findByPk(req.params.id);
   try {
@@ -49,13 +50,29 @@ router.put("/alunos/:id", async (req, res) => {
       await Aluno.update(
         { nome, dataNasc, telefone, email, numMatr },
         { where: { id: req.params.id } }
-      )
+      );
     } else {
-      res.status(404).json({ message: "aluno não foi encontrado" })
+      res.status(404).json({ message: 'aluno não foi encontrado' });
     }
-
   } catch (err) {
-    res.status(500).json({ message: "Um erro aconteceu!" });
+    res.status(500).json({ message: 'Um erro aconteceu!' });
   }
 });
+
+router.delete('/alunos/:id', async (req, res) => {
+  const { id } = req.params;
+  const aluno = await Aluno.findOne({ where: { id } });
+  try {
+    if (aluno) {
+      await Aluno.destroy({ where: { id } });
+      res.status(200).json({ message: 'Aluno removido con sucesso.',aluno });
+    } else {
+      res.status(404).json({ message: 'Aluno não encontrado.' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Um erro inesperado aconteceu.' });
+  }
+});
+
 module.exports = router;
