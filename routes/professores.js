9355393b3178835c332d9165d8718f17a9a0020c
jhonasjhones <jhonajhone222@gmail.com>
2023-04-20 09:39:17 -0300

    Adicionado funcionalidade de edição(put) de professor

--- routes/professores.js
+++ routes/professores.js
@@ -3,6 +3,24 @@ const {Router} = require("express")
 
 const router = Router();
 
+
+router.get("/professores", async (req,res) => {
+    const listaProfessores = await Professor.findAll();
+    res.json(listaProfessores);
+});
+
+router.get("/professor/:id", async (req, res) => {
+    const professor = await Professor.findOne({ where: { id:req.params.id }
+    })
+
+    if(professor){
+        res.json(professor);
+    } else {
+        res.status(404).json({message:"Professor não cadastrado!"})
+    }
+
+})
+
 router.post("/professores", async (req, res) => {
     const {nome, dataNasc, telefone, email} = req.body;
 
@@ -17,6 +35,26 @@ router.post("/professores", async (req, res) => {
         console.log(err);
         res.status(400).json({message: "Não foi possível cadastrar um novo professor."});
     }
+
+})
+
+router.put("/professor/:id", async (req, res) => {
+    const {nome, dataNasc, telefone, email} = req.body;
+    const {id} = req.params;
+
+try {
+    const professor = await Professor.findOne({ where: { id } });
+    if(professor){
+        await professor.update({nome, dataNasc, telefone, email});
+        res.status(200).json({message:"Professor editado com sucesso!"})
+    } else {
+        res.status(404).json({message:"Professor não encontrado!"})
+    }
+
+} catch(err) {
+    console.error(err)
+    res.status(500).json ({message: "Um erro aconteceu!"})
+}
 
 });
 
